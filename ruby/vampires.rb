2475bd33b06c3ec08068b076782b16f3bf9433e3
marissatraina <marissatraina@gmail.com>
2016-08-05 16:35:07 -0400

    Input vampire detector questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,4 +2,11 @@
 
 puts "Hi there, new employee! What is your name?"
 name = gets.chomp
-puts ""
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
