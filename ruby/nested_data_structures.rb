3f1f5b11ff2ce78bbbd50f58cace7d522b366d43
marissatraina <marissatraina@gmail.com>
2016-08-14 03:50:50 -0400

    Initial nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,53 @@
+hogwarts = {
+  gryffindor: {
+    pretty_name: 'Gryffindor',
+    total_points: {
+      first_semester: 250,
+      second_semester: 777
+    },
+    students: [
+      "Hermione Granger",
+      "Harry Potter",
+      "Ron Weasley"
+    ]
+  },
+
+  slytherin: {
+    pretty_name: 'Slytherin',
+    total_points: {
+      first_semester: 324,
+      second_semester: 666
+    },
+    students: [
+      "Draco Malfoy",
+      "Gregory Goyle",
+      "Vincent Crabbe"
+    ]
+  },
+
+  ravenclaw: {
+    pretty_name: 'Ravenclaw',
+    total_points: {
+      first_semester: 530,
+      second_semester: 689
+    },
+    students: [
+      "Luna Lovegood",
+      "Cho Chang",
+      "Padma Patil"
+    ]
+  },
+
+  hufflepuff: {
+    pretty_name: 'Hufflepuff',
+    total_points: {
+      first_semester: 220,
+      second_semester: 344
+    },
+    students: [
+      "Cedric Diffory",
+      "Nymphadora Tonks",
+      "Edward Lupin"
+    ]
+  }
+}
