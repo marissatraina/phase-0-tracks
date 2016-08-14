a374977fcc9908953a2a8f27b5e5fab20b4fadbd
marissatraina <marissatraina@gmail.com>
2016-08-14 02:28:57 -0400

    Complete Release 3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,17 +6,48 @@ puts "Welcome to Interior Designers NYC! Please input the following useful clien
 
 #  -Prompt client's first name
 puts "First Name:"
-form[:first_name] = gets.chomp
+#      -set equal to symbol
+form[:First_Name] = gets.chomp
 
-#      -set equal to symbol
 #  -Prompt client's last name
+puts "Last Name:"
 #      -set equal to symbol
+form[:Last_Name] = gets.chomp
+
 #  -Prompt number of children
+puts "Number of Children:"
 #      -convert input to integer
+form[:Children] = gets.chomp.to_i
+
 #  -Repeat for other info
+puts "Number of Rooms:"
+form[:Rooms] = gets.chomp.to_i
+
+puts "Decor Theme:"
+form[:Decor_Theme] = gets.chomp.to_s
+
 #  -Print information form
+form.each do |key, value|
+  puts key.to_s + ': ' + value.to_s
+end
+
 #  -Give client option to update
+puts "Which information would you like to update? If no edits are necessary, please type 'no' "
+edits = gets.chomp
+
 #  -Depending on client input
-#      -no edits --> print thank you
 #      -yes edits --> allow for symbol substitution
-#        -exit and print thank you
+#      -no edits --> print thank you
+#        -exit and print thank you along with final version
+  if edits != "no"
+    edits = edits.to_sym
+    puts "Please input your edit to #{edits}:"
+    form[edits] = gets.chomp
+    puts "Thank you for your time! Here is the final verion of your form:"
+  else puts "Thank you for your time! Here is the final verion of your form:"
+  end
+
+form.each do |key, value|
+  puts key.to_s + ': ' + value.to_s
+end
+
