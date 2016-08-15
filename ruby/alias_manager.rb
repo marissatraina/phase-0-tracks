e71a5034f7cb204ca8344b281d613051813e5efd
marissatraina <marissatraina@gmail.com>
2016-08-15 10:08:44 -0400

    define alias_generator method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,14 @@
 #Alias Manager
 # create vowel string for comparison
 $vowels = 'aeiou'
+
 # define method vowel_check to search through string for vowels
 def vowel_check(character)
   (char =~ /[aeiou]/) != nil
 end
-# define method vowels_next to replace vowels
-def vowels_next (character)
+
+# define method vowel_next to replace vowels
+def vowel_next (character)
   if character == 'u'
     character = 'a'
   else 
@@ -14,11 +16,28 @@ def vowels_next (character)
   end
   return character
 end
+
 # define method alias_generator
-  # employ the vowel_check method
-  # employ the vowels_next method on string
-  # split up string into two pieces
-  # swap first and last
+def alias_generator(name)
+  #lowercase all strings in database
+  database = name.downcase.split('')
+
+  #iterate through database
+  database.length.times do |i|
+  
+  # employ the vowel_check method & vowel_next method on names
+  if vowel_check(database[i])
+    database[i] = vowel_next(database[i])
+  else database[i] = database[i]
+  end 
+  
+  # split up string into two pieces & swap first and last name
+  swap = database.join('').split(' ').reverse!
+  swap[0].capitalize!
+  swap[1].capitalize!
+
+  return swap.join(' ')
+end
 # Create alias database to store aliases
 #Create User Interface
   # asks user to input first and last name at once
