dffd7f5db169d5468a25b2b1f583077778c9de55
marissatraina <marissatraina@gmail.com>
2016-08-15 10:16:07 -0400

    Add User Interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -38,9 +38,20 @@ def alias_generator(name)
 
   return swap.join(' ')
 end
+
 # Create alias database to store aliases
+alias_database = {}
+
 #Create User Interface
   # asks user to input first and last name at once
+  puts "\n" + "Please enter the agent full name(s) into the alias generator:"
+  agent_name = gets.chomp
   # loops through until user inputs 'exit'
+  while agent_name != 'exit'
+    puts "\n" + "Alias: " + alias_generator(agent_name)
   # employs alias_generator and stores result in database
+  agent_database[agent_name] = alias_generator(agent_name)
+
+  puts "\n" + "Enter another name into the alias generator or enter 'exit' to close the generator:"
+  agent_name = gets.chomp
 #print database
