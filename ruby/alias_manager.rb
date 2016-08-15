38e26591b88b5f4659f20b4bc191f17bff8569ef
marissatraina <marissatraina@gmail.com>
2016-08-15 10:00:03 -0400

    define vowel_check and vowel_next method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,7 +2,18 @@
 # create vowel string for comparison
 $vowels = 'aeiou'
 # define method vowel_check to search through string for vowels
-# define method vowels_next to replace vowels 
+def vowel_check(character)
+  (char =~ /[aeiou]/) != nil
+end
+# define method vowels_next to replace vowels
+def vowels_next (character)
+  if character == 'u'
+    character = 'a'
+  else 
+    character = $vowels[$vowels.index(character) + 1]
+  end
+  return character
+end
 # define method alias_generator
   # employ the vowel_check method
   # employ the vowels_next method on string
