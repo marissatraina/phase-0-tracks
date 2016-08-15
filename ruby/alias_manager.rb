e9cea3d50aa3af1235af1e84a66d574cb18a51be
marissatraina <marissatraina@gmail.com>
2016-08-15 09:54:38 -0400

    initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+#Alias Manager
+# create vowel string for comparison
+#$vowels = 'aeiou'
+# define method vowel_check to search through string for vowels
+# define method vowels_next to replace vowels 
+# define method alias_generator
+  # employ the vowel_check method
+  # employ the vowels_next method on string
+  # split up string into two pieces
+  # swap first and last
+# Create alias database to store aliases
+#Create User Interface
+  # asks user to input first and last name at once
+  # loops through until user inputs 'exit'
+  # employs alias_generator and stores result in database
+#print database
