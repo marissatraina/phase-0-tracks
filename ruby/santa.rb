932f70d7d46c565ad20d49ce337ffba5e42126f1
marissatraina <marissatraina@gmail.com>
2016-08-21 01:41:26 -0400

    Add diverse initializations

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,8 +6,8 @@ class Santa
   # Initialize method
   def initialize 
     puts "Initializing Santa instance..."
-    @gender = gender
-    @ethnicity = ethnicity
+    @gender = "N/A"
+    @ethnicity = "N/A"
   end 
 # attributes not passed in on initialization
   @reindeer_ranking = [
@@ -22,7 +22,7 @@ class Santa
     "Blitzen"
   ]
   @age = 0
-  
+
 # Define Speak Method
 # => prints ho ho message
   def speak
@@ -39,6 +39,41 @@ class Santa
 end
 
 # Test Code 
-nick = Santa.new 
+  # example genders for looping
+  example_genders = [
+    "agender", 
+    "female", 
+    "bigender", 
+    "male", 
+    "female", 
+    "gender fluid", 
+    "N/A"
+  ]
+
+  # example ethnicities for looping
+  example_ethnicities = [
+    "black", 
+    "Latino", 
+    "white", 
+    "Japanese-African", 
+    "prefer not to say", 
+    "Mystical Creature (unicorn)", 
+    "N/A"
+  ]
+
+  puts "While talking to some Santas at SantaCon..."
+  example_genders.each do |gender|
+    puts "One santa identified as #{gender}."
+  end
+
+  puts "Also, we met..."
+  example_ethnicities.each do |ethnicity|
+    puts "a #{ethnicity} santa!"
+  end
+  puts "& more!"
+
+puts "What a diverse & beautiful group of santas!"
+
+nick = Santa.new
 nick.speak
 nick.eat_milk_and_cookies("ginger snap")
