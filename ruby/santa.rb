1bdcb11f3dff313673faf82bc2247cf2d4519023
marissatraina <marissatraina@gmail.com>
2016-08-21 00:23:17 -0400

    Input pseudocode

---
+++ ruby/santa.rb
@@ -0,0 +1,10 @@
+# SantaCon Simulator 
+
+# Define Santa Class
+# Define Speak Method
+#   Prints Ho ho message
+# Define eat_milk_and_cookies methdo
+#   Ask for user input --> cookie type 
+#   Prints good cookie message to user
+# Initialize method
+# Test Code 
