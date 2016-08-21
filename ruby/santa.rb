c4a03b355f3976056dd603db6aef2c74116cc410
marissatraina <marissatraina@gmail.com>
2016-08-21 00:33:01 -0400

    Add speak and cookie method and test code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,28 @@
 # SantaCon Simulator 
 
 # Define Santa Class
+class Santa
+
 # Define Speak Method
-#   Prints Ho ho message
-# Define eat_milk_and_cookies methdo
-#   Ask for user input --> cookie type 
+# => prints ho ho message
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+# Define eat_milk_and_cookies method
 #   Prints good cookie message to user
+
+  def eat_milk_and_cookies(flavor)
+    puts "That was a good #{flavor}!"
+  end
+
 # Initialize method
+  def initialize 
+    puts "Initializing Santa instance..."
+  end 
+end
+
 # Test Code 
+nick = Santa.new 
+nick.speak
+nick.eat_milk_and_cookies("ginger snap")
