d9b3ad0dd3af2eabd050a129f27964f8e14c9f1c
marissatraina <marissatraina@gmail.com>
2016-08-22 00:06:56 -0400

    Pseudocode for Band class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,1 +1,32 @@
 # Band Class
+
+#--Attributes--
+# genre
+# name
+# hometown
+# members 
+
+# Arrays/Hashes
+# empty band_members hash
+# instrument_options array
+# genre_options array
+# hometown_options array
+# member_names array
+
+#--Methods--
+# band_members (between 2 and 9)
+  # randomly chooses a number between 2 and 9
+  # input into band member hash
+
+# assign_instruments(argument -> number of members)
+  #randomly choose number of instruments depending on number of members
+  # input hash with members and instruments
+
+# genre_generator
+  # select randomly from genre_options
+
+# kick_out_member(name)
+  #delete name string from band_members hash
+
+# hometown_generator
+  #randomly select from hometown_options
