c5bf0258a27120556d9007189de5b5938fee0ef5
marissatraina <marissatraina@gmail.com>
2016-08-24 16:49:07 -0400

    Initial commit and complete release 0

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["blue", "green", "brown", "white"];
+var names = ["Frou Frou", "Candace", "Jeremy", "Butterscotch"];
+
+colors.push("auburn");
+names.push("Tulip");
+
+//Driver Code
+
+console.log(colors);
+console.log(names);
