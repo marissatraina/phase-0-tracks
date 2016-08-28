a5176a3034e259613aefd80f9097afc982665e3c
marissatraina <marissatraina@gmail.com>
2016-08-28 14:10:54 -0400

    Complete pseudocode for all 3 releases

--- js/algos.js
+++ js/algos.js
@@ -1,20 +1,42 @@
 // Release 0: Find the Longest Phrase
 
   // Define longest phrase method that takes array as argument
+    // Loop through arraying depending on length of array
     // Iterate through array of words to determine length of each word
-    // Stop iteration when there is one word left in array
+    // Stop iteration when there is one word left in array or more
+      //than one phrase is the same length
     // Compare length of word to length of preceding word
-    // Delete phrase that is shorter
-    // return/print last remaining phrase in array
+    // Delete phrase, if phrase is shorter
+    // return/print last remaining phrase(s) in array
 
-  // Driver Code
+  // Driver Code --> tests on a few different arrays
+
 
 // Release 1: Find a Key-Value Match
 
-  // Define in_common method that takes two hashes for arguments
-    // determine length of hash
-    // loop through by index and to see if the value of each is equal
+  // Define in_common method that takes two objects for arguments
+    // determine length of objects
+    // loop through by index
+    // set keys equal to one another to see if the value of each key is equal 
     // if at least one index matches return true
+
+  // Driver Code --> tess for both true and false outcomes
 
 
 // Release 2: Generate Random Test Data
+
+  // Random Test Data Generator Method that takes number of strings
+  // in array as argument
+  // create empty array
+  // create alphabet array
+  // create number array 1 to 10
+  // for each string - 
+  // choose between 1 and 10 letters per string randomly by sampling
+    // from number array
+  // iteratively sample random number of letters from alphabet string --> store
+    // join letters together to create string --> store in array
+  // repeat until array is filled according to argument number
+
+  // Driver Code --> 10 times iteration that builds array and performs
+  // longest word method on array
+
