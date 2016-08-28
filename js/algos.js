aa2ca90e8e9d0cf5943ea96e7aedd995318b90b5
marissatraina <marissatraina@gmail.com>
2016-08-28 02:15:47 -0400

    Initial commit and pseudocode

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+// Release 0: Find the Longest Phrase
+
+  // Define longest phrase method that takes array as argument
+    // Iterate through array of words to determine length of each word
+    // Stop iteration when there is one word left in array
+    // Compare length of word to length of preceding word
+    // Delete phrase that is shorter
+    // return/print last remaining phrase in array
+
+  // Driver Code
+
+// Release 1: Find a Key-Value Match
+
+  // Define in_common method that takes two hashes for arguments
+    // determine length of hash
+    // loop through by index and to see if the value of each is equal
+    // if at least one index matches return true
+
+
+// Release 2: Generate Random Test Data
