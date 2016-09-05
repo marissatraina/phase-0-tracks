eae2cb4e1b8f0250d6a29cc6d558bdb6654be3c3
marissatraina <marissatraina@gmail.com>
2016-09-04 20:28:41 -0400

    Fix bug and downcase input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,24 +7,24 @@ puts "Welcome to Interior Designers NYC! Please input the following useful clien
 #  -Prompt client's first name
 puts "First Name:"
 #      -set equal to symbol
-form[:First_Name] = gets.chomp
+form[:first_name] = gets.chomp
 
 #  -Prompt client's last name
 puts "Last Name:"
 #      -set equal to symbol
-form[:Last_Name] = gets.chomp
+form[:last_name] = gets.chomp
 
 #  -Prompt number of children
 puts "Number of Children:"
 #      -convert input to integer
-form[:Children] = gets.chomp.to_i
+form[:children] = gets.chomp.to_i
 
 #  -Repeat for other info
 puts "Number of Rooms:"
-form[:Rooms] = gets.chomp.to_i
+form[:rooms] = gets.chomp.to_i
 
 puts "Decor Theme:"
-form[:Decor_Theme] = gets.chomp.to_s
+form[:decor_theme] = gets.chomp.to_s
 
 #  -Print information form
 form.each do |key, value|
@@ -33,17 +33,25 @@ end
 
 #  -Give client option to update
 puts "Which information would you like to update? If no edits are necessary, please type 'no' "
-edits = gets.chomp
+edits = gets.chomp.downcase
 
 #  -Depending on client input
 #      -yes edits --> allow for symbol substitution
 #      -no edits --> print thank you
 #        -exit and print thank you along with final version
   if edits != "no"
-    edits = edits.to_sym
-    puts "Please input your edit to #{edits}:"
-    form[edits] = gets.chomp
+
+      case edits
+        when "first_name" || "last_name" || "decor_theme"
+          puts "Please input your edit to #{edits}:"
+          form[edits.to_sym] = gets.chomp
+        when "children" || "rooms"
+          form[edits.to_sym] = gets.chomp.to_i
+        else puts "Sorry that's not an option!"
+      end
+  
     puts "Thank you for your time! Here is the final verion of your form:"
+
   else puts "Thank you for your time! Here is the final verion of your form:"
   end
 
