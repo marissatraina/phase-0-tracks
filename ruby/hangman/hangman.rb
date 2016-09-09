fddb60babd7bd3f3a67ea0bfdf15a5d712563a80
marissatraina <marissatraina@gmail.com>
2016-09-09 18:22:44 -0400

    Add word_choice method

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,22 +1,77 @@
 #---Hangman Class---
 
+# define hangman class
+
+#   initialize with 
+#     word variable 
+#     empty letters array
+#     empty blank spaces array
+#     empty guess hash
+
+#   word_choice method
+#     determines length of word
+#     if word length <= 11 words or >= 3
+#       stores chosen word into word variable
+#     splits word into letters
+#     store letters into array
+  
+#   number_of_guesses method
+#     allows for 7 guesses if the word length is <= 5 letters
+#       stores 7 guess symbols in guess hash
+#     allows for 9 guesses if the word length is <= 7 letters
+#       stores 9 guess symbols in guess hashes
+#     allows for 12 guesses if the word length is <= 11 letters
+#       stores 12 guess symbols in guess hash
+    
+#   guess method
+#     if input string length = 1
+#       stores guess into respective hash symbol
+#     iterates through letters array to check for matches
+#     if letter doesn’t match any previously stored symbols
+#       & if letter doesn’t match letters in word
+#     guess is considered wrong
+    
+
+#   draw method
+#     for every wrong guess it draws a part of stick figure
+
+#   progress method
+#     displays initial blanks to guessing player
+#     for every correct guess
+#     returns hash array with filled in correct letters in word
+
 class Hangman
 
+  attr_accessor :word
+  attr_reader :blanks, :word
+
   def initialize 
-    @word = word
+    @word = ""
     @letters = []
     @blanks = []
     @guesses = {}
   end
 
   def word_choice(word)
-    if word.length <= 11 && >= 3
-      @word = word 
+    case  
+    when word.length <= 11 && word.length >= 3
+      @word = word
+      word.length.times do 
+        @blanks << " _"
+      end
+      p "*Draws on chalkboard*:#{@blanks.join('')}"
+    when word.length < 3
+      p "That word is too short to play hangman with!"
+    when word.length > 11
+      p "That word is too long to play hangman with!"
     end
   end
-
 
 end
 
 #---Driver Code---
-# one = Hangman.new
+# test = Hangman.new
+# test.word_choice("bluets")
+
+
+
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -2,5 +2,22 @@ require_relative 'hangman'
 
 
 describe Hangman do
-  let(:one) { Hangman.new }
+  let(:test) { Hangman.new }
 
+it "creates a blank space array of word length" do
+  expect(test.word_choice("bluets")).to eq "*Draws on chalkboard*: _ _ _ _ _ _"
+end
+
+it "does not allow words shorter than 3 letters" do
+  expect(test.word_choice("ok")).to eq "That word is too short to play hangman with!"
+end
+
+it "does not allow words longer than 11 letters" do
+  expect(test.word_choice("antidisestablishmentarianism")).to eq "That word is too long to play hangman with!"
+end
+
+it "allows 7 guesses if the word is less <= 5 letters " do
+  expect(test.word_choice("bluets")).to eq "That word is too long to play hangman with!"
+end
+
+end
