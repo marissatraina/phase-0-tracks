7643ebcaddff0b013a394489c16ecea247e24ece
marissatraina <marissatraina@gmail.com>
2016-09-30 00:29:48 -0400

    Complete Bonus Question

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -26,14 +26,20 @@ end
 # write a GET route that retrieves
 # all student data
 get '/students' do
-  students = db.execute("SELECT * FROM students")
-  response = ""
-  students.each do |student|
-    response << "ID: #{student['id']}<br>"
-    response << "Name: #{student['name']}<br>"
-    response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
+  campus = params[:campus]
+
+  if campus
+    students = db.execute("SELECT * FROM students WHERE campus=?", [campus])
+  else
+    students = db.execute("SELECT * FROM students")
   end
+    response = ""
+    students.each do |student|
+      response << "ID: #{student['id']}<br>"
+      response << "Name: #{student['name']}<br>"
+      response << "Age: #{student['age']}<br>"
+      response << "Campus: #{student['campus']}<br><br>"
+    end
   response
 end
 
@@ -69,3 +75,5 @@ get '/:num1/plus/:num2' do
  total = total.to_s
  "#{num1} + #{num2} = #{total}"
 end
+
+  
