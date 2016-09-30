3c3d9059f6af21b3f1ec4f2d995d1793d0fd5c43
marissatraina <marissatraina@gmail.com>
2016-09-30 00:17:12 -0400

    Update get addition

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -62,6 +62,10 @@ get '/great_job' do
   end
 end
 
-get '/num_1/plus/num_2' do
-
+get '/:num1/plus/:num2' do
+ num1 = params[:num1]
+ num2 = params[:num2]
+ total = num1.to_i + num2.to_i
+ total = total.to_s
+ "#{num1} + #{num2} = #{total}"
 end
